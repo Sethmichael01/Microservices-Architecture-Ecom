702d5524005cabd2e9219aded2861a319d5e6284
Longe Michael Seth <sethmichael1.sm@gmail.com>
2022-04-30 07:17:46 +0100

    Intial Commit: Product API setup

---
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public double Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Mango.Services.ProductAPI/Repository/IProductRepo.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepo
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int ProductId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
